a61fc341ce630128acd8c77809cc900f505c203c
이지윤 <ij447504@gmail.com>
2026-02-04 15:50:53 +0900

    20260204 15:50:00 인증과권한

--- src/main/java/com/example/boardv1/board/BoardController.java
+++ src/main/java/com/example/boardv1/board/BoardController.java
@@ -3,6 +3,8 @@ package com.example.boardv1.board;
 import java.io.IOException;
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.boardv1.user.User;
+
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,23 +26,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class BoardController {
 
     private final BoardService boardService;
+    private final HttpSession session;
 
     // body : title=title7&content=content7 (x-www-form)
     @PostMapping("/boards/save")
-    public String save(BoardRequest.SaveOrUpdateDTO reqDTO) throws IOException {
-        boardService.게시글쓰기(reqDTO.getTitle(), reqDTO.getContent());
+    public String save(BoardRequest.SaveOrUpdateDTO reqDTO) {
+        // 인증(v). 권한(x)
+        // id 정보가 없는데 User 정보를 쓰는 것이 맞나? title과 content를 사용해야 하는 것이 아닌가?
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null)
+            throw new RuntimeException("인증되지 않았습니다.");
+
+        boardService.게시글쓰기(reqDTO.getTitle(), reqDTO.getContent(), sessionUser);
         return "redirect:/";
     }
 
     // body : title=하하하&content=호호호호호
     @PostMapping("/boards/{id}/update")
     public String update(@PathVariable("id") int id, BoardRequest.SaveOrUpdateDTO reqDTO) {
-        boardService.게시글수정(id, reqDTO.getTitle(), reqDTO.getContent());
+        // 인증(v). 권한(v)
+
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null)
+            throw new RuntimeException("인증되지 않았습니다.");
+
+        boardService.게시글수정(id, reqDTO.getTitle(), reqDTO.getContent(), sessionUser.getId());
         return "redirect:/boards/" + id;
     }
 
     @GetMapping("/")
     public String index(HttpServletRequest req) {
+        // 인증(x). 권한(x)
         List<Board> list = boardService.게시글목록();
         req.setAttribute("models", list);
         return "index";
@@ -45,28 +64,45 @@ public class BoardController {
 
     @GetMapping("/boards/save-form") // 게시글 화면에 갈거임 .CV
     public String saveForm() {
+        // 인증이안됐으니 쓰지말고 로그인화면을 보내기
+        // 인증(v). 권한(x)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null)
+            throw new RuntimeException("인증되지 않았습니다.");
+
         return "board/save-form";
     }
 
     @GetMapping("/boards/{id}/update-form")
     public String updateForm(@PathVariable("id") int id, HttpServletRequest req) {
-        Board board = boardService.상세보기(id);
+        // 인증(v). 권한(v)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            throw new RuntimeException("update-form : 인증되지 않았습니다. 로그인 후 이용해 주세요.");
+        }
+        Board board = boardService.수정폼게시글정보(id, sessionUser.getId());
         req.setAttribute("model", board);
         return "board/update-form";
     }
 
     @GetMapping("/boards/{id}")
     public String detail(@PathVariable("id") int id, HttpServletRequest req) {
-        // 중괄호 안에 있는 id 를 패싱해서 받아줌
-        Board board = boardService.상세보기(id);
-        // 가방담기
-        req.setAttribute("model", board);
+        // 인증(x). 권한(x)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        Integer sessionUserId = sessionUser == null ? null : sessionUser.getId();
+        BoardReponse.DetailDTO dto = boardService.상세보기(id, sessionUserId);
+        req.setAttribute("model", dto);
         return "board/detail";
     }
 
     @PostMapping("/boards/{id}/delete")
     public String delete(@PathVariable("id") int id) {
-        boardService.게시글삭제(id);
+        // 인증(v). 권한(v)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null)
+            throw new RuntimeException("인증되지 않았습니다.");
+
+        boardService.게시글삭제(id, sessionUser.getId());
         return "redirect:/";
     }
 
--- src/main/java/com/example/boardv1/board/BoardService.java
+++ src/main/java/com/example/boardv1/board/BoardService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.boardv1.board.BoardRequest.SaveOrUpdateDTO;
+import com.example.boardv1.user.User;
+
 import lombok.RequiredArgsConstructor;
 
+// 책임 : 트랜잭션관리, DTO 만들기, 권한체크(DB정보가 필요)
 @RequiredArgsConstructor
 @Service
 public class BoardService {
@@ -17,16 +21,33 @@ public class BoardService {
         return boardRepository.findAll();
     }
 
-    public Board 상세보기(int id) {
-        return boardRepository.findById(id)
+    public Board 수정폼게시글정보(int id, int sessionUserId) {
+        Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없어요"));
+
+        if (sessionUserId != board.getUser().getId())
+            throw new RuntimeException("삭제할 권한이 없습니다.");
+
+        return board;
     }
 
+    public BoardReponse.DetailDTO 상세보기(int id, Integer sessionUserId) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없어요"));
+
+        return new BoardReponse.DetailDTO(board, sessionUserId);
+    }// 괄호해서 DTO로 묶은 것 DTO잘하면 프론트가 좋아해영영
+
     @Transactional // update, delete, insert 할때 붙이세요!!
-    public void 게시글수정(int id, String title, String content) {
-        //
+    public void 게시글수정(int id, String title, String content, int sessionUserId) {
+
         Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("수정할 게시글을 찾을 수 없어요"));
+
+        if (sessionUserId != board.getUser().getId())
+            throw new RuntimeException("수정할 권한이 없습니다.");
+
+        // 권한 있는 경우 내용 수정
         board.setTitle(title);
         board.setContent(content);
     }
@@ -34,11 +55,12 @@ public class BoardService {
     // 원자성(모든게 다되면 commit, 하나라도 실패하면 rollback)
     // 트랜잭션 종료시 flush 됨.
     @Transactional
-    public void 게시글쓰기(String title, String content) {
+    public void 게시글쓰기(String title, String content, User sessionUser) {
         // 1. 비영속 객체
         Board board = new Board();
         board.setTitle(title);
         board.setContent(content);
+        board.setUser(sessionUser);
 
         System.out.println("before persist " + board.getId());
 
@@ -49,11 +71,15 @@ public class BoardService {
     }
 
     @Transactional
-    public void 게시글삭제(int id) {
+    public void 게시글삭제(int id, int sessionUserId) {
         // 영속화
         Board board = boardRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("삭제할 게시글을 찾을 수 없어요"));
+
+        if (sessionUserId != board.getUser().getId())
+            throw new RuntimeException("삭제할 권한이 없습니다.");
+
         boardRepository.delete(board);
-    } // 자동 flush
+    }
 
 }
